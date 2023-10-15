b1d3839cba69bd028c0ba01b00a8a7b0b5cdaf43
ssongjayh <thewinner1st@naver.com>
2023-10-15 23:35:51 +0900

    feat: exception package
    - 이메일 중복 Exception 추가

---
+++ src/main/java/maetanTwosome/trs/exception/member/DuplicateEmailException.java
@@ -0,0 +1,9 @@
+package maetanTwosome.trs.exception.member;
+
+public class DuplicateEmailException {
+
+    private static final String MESSAGE = "이미 사용 중인 이메일입니다.";
+
+    public DuplicateEmailException() {
+    }
+}
